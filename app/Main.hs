a262ae66deb23acf9e0dbc3d63aae1e00dbad980
asweingarten <asweingarten@gmail.com>
2018-12-01 20:30:17 -0800

    Paint a coloured pixel in a stroked box

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE RecordWildCards  #-}
 
 module Main where
+
+import Lib
 
 import Graphics.Rendering.Cairo
 import qualified Data.Vector as V
@@ -10,43 +11,18 @@ import Data.Random.Distribution.Uniform
 import Data.Random.Source.PureMT
 
 import Data.Foldable
-import Data.Colour.RGBSpace
-import Data.Colour.RGBSpace.HSV
 import Control.Monad.State
 import Control.Monad.Reader
-
-data World = World 
-  { worldWidth  :: Double
-  , worldHeight :: Double
-  , scaleFactor :: Double
-  }
-
-type Generate a  = StateT PureMT (Reader World) a
 
 runGenerate :: World -> PureMT -> Generate a -> a
 runGenerate world rng scene = 
   (flip runReader world) . (>>= (return . fst)) . (flip runStateT rng) $ scene
 
-hsva :: Double -> Double -> Double -> Double -> Render ()
-hsva h s v = setSourceRGBA channelRed channelGreen channelBlue
-        where RGB{..} = hsv h s v
-
-eggshell :: Double -> Render ()
-eggshell = hsva 71 0.13 0.96
-
-bg :: Generate (Render ())
-bg = do
-  (World w h _) <- ask
-  return $ do
-    eggshell 1
-    rectangle 0 0 w h
-    fill
-
 sketch :: State Double (Generate (Render ()))
 sketch = do
   return $ do
-    rs <- sequence [bg]
-    return $ foldr1 (const) rs
+    rs <- sequence [bg, strokeSquare, fillCircle]
+    return $ foldr1 (>>) rs
 
 writeSketch :: World -> PureMT -> String -> Generate (Render ()) -> IO()
 writeSketch world rng path sketch = do
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,66 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE RecordWildCards  #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import Graphics.Rendering.Cairo
+import Data.RVar
+import Data.Random.Distribution.Uniform
+import Data.Random.Source.PureMT
+import Linear.V2
+
+
+import Data.Colour.RGBSpace
+import Data.Colour.RGBSpace.HSV
+import Control.Monad.State
+import Control.Monad.Reader
+
+data World = World 
+  { worldWidth  :: Double
+  , worldHeight :: Double
+  , scaleFactor :: Double
+  }
+
+type Generate a  = StateT PureMT (Reader World) a
+
+hsva :: Double -> Double -> Double -> Double -> Render ()
+hsva h s v = setSourceRGBA channelRed channelGreen channelBlue
+        where RGB{..} = hsv h s v
+
+eggshell :: Double -> Render ()
+eggshell = hsva 71 0.13 0.96
+
+darkGunmetal :: Double -> Render ()
+darkGunmetal = hsva 170 0.30 0.16
+
+englishVermillion :: Double -> Render ()
+englishVermillion = hsva 355 0.68 0.84
+
+bg :: Generate (Render ())
+bg = do
+  (World w h _) <- ask
+  return $ do
+    eggshell 1
+    rectangle 0 0 w h
+    fill
+
+strokeSquare :: Generate (Render ())
+strokeSquare = do
+  (World w h _) <- ask
+  return $ do
+    newPath
+    darkGunmetal 1
+    rectangle (w/5) (h/5) (3*w/5) (3*h/5)
+    stroke 
+
+fillCircle :: Generate (Render ())
+fillCircle = do
+  (World w h _) <- ask
+  x <- sampleRVar $ uniform (0 :: Double) (1 :: Double)
+  y <- sampleRVar $ uniform (0 :: Double) (1 :: Double)
+  let originX = (w/5) + x*(3*w/5)
+  let originY = (h/5) + y*(3*h/5)
+  let cy = y*(h/4) + (h/2)
+  return $ do
+    englishVermillion 1 
+    rectangle originX originY 1 1
+    fill
